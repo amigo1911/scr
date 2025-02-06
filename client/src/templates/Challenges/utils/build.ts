3709a875f3a68df23e2087f2bb47d7de1345cc7c
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-02-06 09:22:10 +0100

    fix: only load Enzyme if React16 is used

--- client/src/templates/Challenges/utils/build.ts
+++ client/src/templates/Challenges/utils/build.ts
@@ -26,7 +26,7 @@ import { WorkerExecutor } from './worker-executor';
 interface BuildChallengeData extends Context {
   challengeType: number;
   challengeFiles?: ChallengeFile[];
-  required: { src: string }[];
+  required: { src?: string }[];
   template: string;
   url: string;
 }
@@ -237,6 +237,10 @@ export async function buildDOMChallenge(
   );
   const isMultifile = challengeFiles.length > 1;
 
+  const requiresReact16 = required.some(({ src }) =>
+    src?.includes('https://unpkg.com/react@16')
+  );
+
   // I'm reasonably sure this is fine, but we need to migrate transformers to
   // TypeScript to be sure.
   const transformers: ApplyFunctionProps[] = (isMultifile && hasJsx
@@ -266,7 +270,7 @@ export async function buildDOMChallenge(
     challengeType: challengeTypes.html,
     build: concatHtml(toBuild),
     sources: buildSourceMap(finalFiles),
-    loadEnzyme: hasJsx,
+    loadEnzyme: requiresReact16,
     error
   };
 }
